4f2f7fd84c63950c04d2d04cc86697c89ffd20e3
Samy <samy@sami.eco>
2024-11-07 13:40:24 +0100

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
+      <Analytics />
       <App />
     </ChakraProvider>
   </StrictMode>
